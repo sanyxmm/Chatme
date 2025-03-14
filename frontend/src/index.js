26b8acb9f9ae4477a0022a385a96ceabb7cf95b4
sanyxmm <iamsanyamchoudhary@gmail.com>
2025-03-15 01:32:12 +0530

    url updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="https://chatbaseapp.onrender.com"
+export const BASE_URL="https://chatme-pzwp.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
