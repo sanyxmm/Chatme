743722cea952434b380308d7c010ecae81f70642
sanyxmm <iamsanyamchoudhary@gmail.com>
2025-03-15 01:21:02 +0530

    done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="http://localhost:5000"
+export const BASE_URL="https://chatbaseapp.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
